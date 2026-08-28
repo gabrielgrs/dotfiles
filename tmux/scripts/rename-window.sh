396207cd20784c8859dce53a922d4784f36cb83a
Gabriel Ribeiro <grxgabriel@gmail.com>
2026-08-28 18:35:19 -0300

    Update tmux config to rename

---
+++ tmux/scripts/rename-window.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+current=$(tmux display-message -p '#W')
+printf 'Window name [%s]: ' "$current"
+
+name=""
+while IFS= read -rsn1 char; do
+    if [[ $char == $'\e' ]]; then
+        exit 0
+    elif [[ $char == $'\n' || $char == $'\r' || -z $char ]]; then
+        break
+    elif [[ $char == $'\x7f' || $char == $'\b' ]]; then
+        if [[ -n $name ]]; then
+            name="${name%?}"
+            printf '\b \b'
+        fi
+    else
+        name+="$char"
+        printf '%s' "$char"
+    fi
+done
+
+tmux rename-window -- "${name:-$current}"
